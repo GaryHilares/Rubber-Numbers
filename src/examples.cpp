a0e84331391ebb1ada3809d28928849f2fd14c6a
Gary Hilares <dev.garyhilares@gmail.com>
2024-04-22 01:23:03 -0700

    Added std::endl to fix output formatting.

--- src/examples.cpp
+++ src/examples.cpp
@@ -5,9 +5,8 @@ int main()
     using namespace RubberNumbers;
     RubberInt test1(RubberInt(45));
     test1 = test1 * RubberInt(-69);
-    std::cout << test1;
+    std::cout << test1 << std::endl;
     RubberFloat test2(RubberFloat(45.4));
     test2 = test2 * RubberFloat(-69);
-    std::cout << test2;
-
+    std::cout << test2 << std::endl;
 }
